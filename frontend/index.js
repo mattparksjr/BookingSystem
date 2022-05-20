25df3b03ec7a56db015db845fd9cb62f9806e9c2
Matthew Parks <mattparksjr@gmail.com>
2022-05-19 23:37:40 -0400

    Fixing stuff

---
+++ frontend/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile(path.join(__dirname, "public/index.html"));
+}
+
+// Exiting(respects platform standards)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+    createWindow()
+
+    // Mac: Open window if none open
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// Hot reloading
+try {
+    require('electron-reloader')(module)
+} catch (_) {}
